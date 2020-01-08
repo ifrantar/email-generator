479ab555350b7bebeaca5590cf4f76f0644587bf
Ivan Frantar <ivan.frantar@stelltec.com>
2020-01-08 14:53:28 +0000

    Included the regulatory and legal stuff into the signature.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,8 @@ function generateSig(){
 	title = document.getElementById('title').value,
 	email = document.getElementById('email').value,
 	phone = document.getElementById('phone').value,
-	city = document.getElementById('city').value;
+	city = document.getElementById('city').value,
+	team = document.getElementById('team').value;
 
 	$searchBlock.find('.employee-name').text(name);
 	$searchBlock.find('.employee-title').text(title);
@@ -25,10 +26,24 @@ function generateSig(){
 		$searchBlock.find('.employee-office-number').text("+44 203 397 5932");
 	} else if (city == "Korea") {
 		$searchBlock.find('.employee-city').text("Three IFC Level 4: 10 Gukjegeumyung-ro, Yeongdeungpo-gu, Seoul, Korea 07326");
-		$searchBlock.find('.employee-office-number').text("Korea's phone number");
+		$searchBlock.find('.employee-office-number').text("+353 1 607 7033");
 	} else if (city == "Stamford") {
 		$searchBlock.find('.employee-city').text("1 Landmark SQ 22nd Floor Stamford, CT, 06901-2603");
 		$searchBlock.find('.employee-office-number').text("+1 203 914 1234");
+	}
+
+	if (team == "Group") {
+		$searchBlock.find('.employee-team').html("This e-mail and any files transmitted with it are CONFIDENTIAL and intended solely for the use of the individual or entity to whom they are addressed.<br> If you have received this e-mail in error please notify the sender. Should you require further assistance please contact:  Stellwagen Group at +353 (1) 607 7000.<br> Stellwagen Group Limited is a private company limited by shares registered in Malta with Company No. C78497<br>Registered Office:  SmartCity Malta SCM01, Suite 502, Ricasoli, Kalkara, Malta <br>Irish Branch: Embassy House, Ballsbridge, Dublin; Company No. 908616");
+	} else if (team == "Capital-US") {
+		$searchBlock.find('.employee-team').html("This e-mail and any files transmitted with it are CONFIDENTIAL and intended solely for the use of the individual or entity to whom they are addressed.<br> If you have received this e-mail in error please notify the sender. Should you require further assistance please contact:  Stellwagen Capital at +353 (1) 607 7000.<br> Stellwagen Capital Limited is registered in Bermuda with Company No. 51478<br> Irish Branch: Embassy House, Ballsbridge, Dublin 4, Ireland, Company No. 908451");
+	} else if (team == "Capital-UK") {
+		$searchBlock.find('.employee-team').html("This e-mail and any files transmitted with it are CONFIDENTIAL and intended solely for the use of the individual or entity to whom they are addressed.<br> If you have received this e-mail in error please notify the sender. Should you require further assistance please contact:  Stellwagen Capital at +353 (1) 607 7000.<br> Stellwagen Capital UK Limited is registered in England with Company No. 11124106<br> Registered Office: 20 North Audley Street, London, United Kingdom, W1K 6WE");
+	}	else if (team == "Seraph") {
+		$searchBlock.find('.employee-team').html("This e-mail and any files transmitted with it are CONFIDENTIAL and intended solely for the use of the individual or entity to whom they are addressed.<br> If you have received this e-mail in error please notify the sender. Should you require further assistance please contact:  Seraph Aviation Management at +353 (1) 607 7000<br> Seraph Aviation Management Limited is registered in Ireland with Company No. 436832.<br> Registered Office: Riverside One, Sir John Rogerson’s Quay, Dublin 2.<br>Directors:   E. O’Reilly │ D. Brennan (Canadian) │ S. Coyle ");
+	} else if (team == "Technology") {
+		$searchBlock.find('.employee-team').html("This e-mail and any files transmitted with it are CONFIDENTIAL and intended solely for the use of the individual or entity to whom they are addressed.<br> If you have received this e-mail in error please notify the sender. Should you require further assistance please contact:  Stelltec at +353 (1) 607 7000.<br> Stellwagen Technology Limited is registered in Bermuda with Company No. 51478<br> Irish Branch: Embassy House, Ballsbridge, Dublin 4, Ireland; Company No. 908481 ");
+	} else if (team == "Korea") {
+		$searchBlock.find('.employee-team').html("This e-mail and any files transmitted with it are CONFIDENTIAL and intended solely for the use of the individual or entity to whom they are addressed.<br> If you have received this e-mail in error please notify the sender. Should you require further assistance please contact:  Stellwagen Korea at +353 (1) 607 7000.<br> Stellwagen Korea Limited is registered in Ireland with Company No. 627177<br> Registered Office: Embassy House, Herbert Park Lane, Ballsbridge, Dublin 4, Ireland<br>Directors:   E. O’Reilly │ Christian Joen (Korean) │ D. Butler<br>");
 	}
 
 
